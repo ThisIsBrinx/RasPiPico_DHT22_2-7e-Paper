262cc14f305ba6ee90e514456931eb51a3b08d82
ThisIsBrinx <phippsroe@hotmail.com>
2023-10-25 22:37:05 +0200

    made code working

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from epaper import EPD_2in7
 # Initialisierung GPIO und DHT22
 dht22_sensor = DHT22(Pin(2, Pin.IN, Pin.PULL_UP))
 
+#adjust your timeout !!!
+timeoutInMinutes = 1
+
 epd = EPD_2in7()
 epd.EPD_2IN7_Init()
 
@@ -16,9 +19,8 @@ marginLeftNumber = 5
 marginTop = 5
 lineSpace = 15
 
-timeoutInMinutes = 1
-timeout= timeoutInMinutes * 100 * 1
-maxMessurement = 48
+timeout = timeoutInMinutes * 1000 * 60
+maxMessurement = 11
 
 request = 1
 
@@ -56,37 +58,23 @@ while True:
     temp.append(newtemp)
     humi.append(newhumi)
 
+    print("request: " + str(request))
+
     while i < request:
+        print("in")
         j = request - i
 
-        print("i: " + str(i))
-        print("request: " + str(request))
-        if i <= 6:
-            print("lower then 6")
-            epd.image1Gray_Landscape.text("-" + str(i) + "h", marginLeftNumber, marginTop + (lineSpace + (lineSpace * i)), epd.black)
-            epd.image1Gray_Landscape.text(str(temp[j-1]) + "C", marginLeftCol1, marginTop + (lineSpace + (lineSpace * i)), epd.black)
-            epd.image1Gray_Landscape.text(str(humi[j-1]) + "%", marginLeftCol2, marginTop + (lineSpace + (lineSpace * i)), epd.black)
-            epd.EPD_2IN7_Display_Landscape(epd.buffer_1Gray_Landscape)
-        elif i == 12:
-            print("12")
-            epd.image1Gray_Landscape.text("-" + str(i) + "h", marginLeftNumber, marginTop + (lineSpace + (lineSpace * 5)), epd.black)
-            epd.image1Gray_Landscape.text(str(temp[j-1]) + "C", marginLeftCol1, marginTop + (lineSpace + (lineSpace * 5)), epd.black)
-            epd.image1Gray_Landscape.text(str(humi[j-1]) + "%", marginLeftCol2, marginTop + (lineSpace + (lineSpace * 5)), epd.black)
-            epd.EPD_2IN7_Display_Landscape(epd.buffer_1Gray_Landscape)
-        elif i == 24:
-            print("24")
-            epd.image1Gray_Landscape.text("-" + str(i) + "h", marginLeftNumber, marginTop + (lineSpace + (lineSpace * 6)), epd.black)
-            epd.image1Gray_Landscape.text(str(temp[j-1]) + "C", marginLeftCol1, marginTop + (lineSpace + (lineSpace * 5)), epd.black)
-            epd.image1Gray_Landscape.text(str(humi[j-1]) + "%", marginLeftCol2, marginTop + (lineSpace + (lineSpace * 5)), epd.black)
-            epd.EPD_2IN7_Display_Landscape(epd.buffer_1Gray_Landscape)
-
-
+        epd.image1Gray_Landscape.text("-" + str(i) + "m", marginLeftNumber, marginTop + (lineSpace + (lineSpace * i)), epd.black)
+        epd.image1Gray_Landscape.text(str(temp[j-1]) + "C", marginLeftCol1, marginTop + (lineSpace + (lineSpace * i)), epd.black)
+        epd.image1Gray_Landscape.text(str(humi[j-1]) + "%", marginLeftCol2, marginTop + (lineSpace + (lineSpace * i)), epd.black)
+        
         i=i+1
 
-    
+    epd.EPD_2IN7_Display_Landscape(epd.buffer_1Gray_Landscape)
     #epd.EPD_2IN7_4Gray_Display(epd.buffer_4Gray)
-       
+    
     if request >= maxMessurement:
+        print("true")
         temp.pop(0)
         humi.pop(0)
         request = request - 1
